609f48c1facf4269e52fd8cc77327e721e283509
redux-anitta.philip <redux-anitta.philip@redux-anittaphilips-MacBook-Air.local>
2025-03-14 10:46:33 +0530

    Changed the wp subnet association to private and added route table for load balancer

--- main/main.tf
+++ main/main.tf
@@ -24,25 +24,39 @@ module "peering_connection" {
   accepter_id         = module.db_vpc.vpc_id
   requester_id        = module.wp_vpc.vpc_id
 }
-module "wp-igw" {
+module "lb-igw" {
   source              = "../modules/igw"
   vpc_id              = module.wp_vpc.vpc_id
   project_environment = var.project_environment
   project_name        = var.project_name
 }
-module "wp_rt" {
+module "lb_rt" {
   source                    = "../modules/rt"
   project_environment       = var.project_environment
   project_name              = var.project_name
   vpc_id                    = module.wp_vpc.vpc_id
   type                      = "public"
   subnet_id                 = module.wp_vpc.public_subnet_ids
-  vpc_peering_connection_id = module.peering_connection.id
-  peer_cidr_block           = var.db_cidr_block
-  internet_gateway_id       = module.wp-igw.igw_id
-  enable_peering            = true
+  vpc_peering_connection_id = null
+  peer_cidr_block           = null
+  internet_gateway_id       = module.lb-igw.igw_id
+  enable_peering            = false
   enable_internet           = true
 }
+module "wp_rt" {
+  source                    = "../modules/rt"
+  project_environment       = var.project_environment
+  project_name              = var.project_name
+  vpc_id                    = module.wp_vpc.vpc_id
+  type                      = "private"
+  subnet_id                 = module.wp_vpc.private_subnet_ids
+  vpc_peering_connection_id = module.peering_connection.id
+  peer_cidr_block           = var.db_cidr_block
+  internet_gateway_id       = null
+  enable_peering            = true
+  enable_internet           = false
+}
+
 module "db_rt" {
   source                    = "../modules/rt"
   project_environment       = var.project_environment
