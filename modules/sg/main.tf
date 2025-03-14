5f1e9155f27cd46199219f4565039e8076ebf1a3
redux-anitta.philip <redux-anitta.philip@redux-anittaphilips-MacBook-Air.local>
2025-03-14 12:21:48 +0530

    Created sg common module

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -7,3 +7,23 @@ resource "aws_security_group" "main" {
     Name = "${var.project_name}-${var.project_environment}-${var.name}"
   }
 }
+resource "aws_vpc_security_group_ingress_rule" "ingress" {
+  for_each          = toset(var.sg_ports)
+  security_group_id = aws_security_group.main.id
+  referenced_security_group_id  = var.sg_reference_id
+  from_port         = each.key
+  ip_protocol       = "tcp"
+  to_port           = each.key
+  tags = {
+    Name = "${var.project_name}-${var.project_environment}-${var.name}"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "egress" {
+  security_group_id = aws_security_group.main.id
+  cidr_ipv4         =  var.egress_cidr_ipv4
+  ip_protocol       = var.egress_port
+  tags = {
+    Name = "${var.project_name}-${var.project_environment}-${var.name}"
+  }
+}
--- modules/sg/variables.tf
+++ modules/sg/variables.tf
@@ -18,3 +18,24 @@ variable "vpc_id" {
   type        = string
   description = "vpc id"
 }
+
+variable "sg_ports" {
+  type        = list(string)
+  description = "sg ports"
+}
+variable "name" {
+  type        = string
+  description = "sg name"
+}
+variable "sg_reference_id" {
+  type        = string
+  description = "sg reference id"
+}
+variable "egress_cidr_ipv4" {
+  type        = string
+  description = "egress cidr ipv4"
+}
+variable "egress_port" {
+  type        = string
+  description = "egress port"
+}
