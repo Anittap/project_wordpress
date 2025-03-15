7a3358eeb169f1a64da40eaa90bee98873d21e34
redux-anitta.philip <redux-anitta.philip@redux-anittaphilips-MacBook-Air.local>
2025-03-15 14:32:34 +0530

    Added datasource to fetch AMI created from packer

--- main/datasource.tf
+++ main/datasource.tf
@@ -2,3 +2,12 @@ data "aws_route53_zone" "main" {
   name         = var.domain_name
   private_zone = false
 }
+data "aws_ami" "packer_ami" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["${var.project_name}-${var.project_environment}-*"]
+  }
+}
